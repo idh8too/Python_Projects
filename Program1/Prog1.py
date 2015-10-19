87dc3d77a6551bcd25cffe40dc57f45616e492cb
idh8too <kristoferard@live.com>
2015-10-18 22:16:07 -0500

    Initial commit

---
+++ Program1/Prog1.py
@@ -0,0 +1,8 @@
+## Program Assignment: 1
+## Program Description: This is a simple program with both
+## input and output.
+
+firstName = input("Enter First Name: ")
+major = input("Enter your major: ")
+print("My name is " + firstName + ".")
+print("My major is " + major + ", and I love to program!")
